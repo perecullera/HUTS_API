540a8fb8c82b3234c57f8b8fcde24566bb06e924
Pere Cullera <cameame@gmail.com>
2016-01-22 20:50:09 +0100

    sorted by street name

--- hutsAPI/views.py
+++ hutsAPI/views.py
@@ -7,7 +7,7 @@ from hutsAPI.models import Building, Hut
 
 
 def index(request):
-        buildings = Building.objects.all()
+        buildings = Building.objects.all().order_by('number','street')
         return render_to_response("index.html",RequestContext(request,
                                              {'huts': buildings}))
 
